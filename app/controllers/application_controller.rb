0567b2d86a351aa5ccd33f4110647419637594e3
ryuki.abe <r.a.nowitzki41@softbank.ne.jp>
2019-12-27 12:00:48 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+  include SessionsHelper
+   
   private
 
   def require_user_logged_in
@@ -7,4 +9,8 @@ class ApplicationController < ActionController::Base
       redirect_to login_url
     end
   end
+  
+  def counts(user)
+    @count_microposts = user.microposts.count
+  end  
 end
