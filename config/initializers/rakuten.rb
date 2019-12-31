941fb0e4fa8f3550dd8a8152a5294296bca9d9d4
ryuki.abe <r.a.nowitzki41@softbank.ne.jp>
2019-12-31 08:30:36 +0000

    search

---
+++ config/initializers/rakuten.rb
@@ -0,0 +1,6 @@
+RakutenWebService.configure do |c|
+  c.application_id = '1075493258268263557'
+  c.affiliate_id = '197f1bec.54714bbc.197f1bed.6b104c89'
+  c.max_retries = 3 # default: 5
+  c.debug = true # default: false
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
+  get 'search', to: 'goods#search'
+
+  resources :goods
+  
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create] do
     member do
